44f5be64c35951154348d36bf9ab12f34c5af1ed
Wagner Junior <wagner.junior@dito.com.br>
2019-07-15 15:20:30 -0300

    implemented firebase utils, included ability to store authenticated users into our firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,39 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyAXfbj5ntDJOZFzyzBFhuMnKALXOc5Tk_I",
-    authDomain: "crwn-dba.firebaseapp.com",
-    databaseURL: "https://crwn-dba.firebaseio.com",
-    projectId: "crwn-dba",
-    storageBucket: "",
-    messagingSenderId: "726829279297",
-    appId: "1:726829279297:web:10ddfdab59ae9f66"
+  apiKey: "AIzaSyAXfbj5ntDJOZFzyzBFhuMnKALXOc5Tk_I",
+  authDomain: "crwn-dba.firebaseapp.com",
+  databaseURL: "https://crwn-dba.firebaseio.com",
+  projectId: "crwn-dba",
+  storageBucket: "",
+  messagingSenderId: "726829279297",
+  appId: "1:726829279297:web:10ddfdab59ae9f66"
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
   }
+  return userRef
+}
 
 firebase.initializeApp(config)
 
