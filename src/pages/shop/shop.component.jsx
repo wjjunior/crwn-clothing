216afb73079fea05ad932bec14474fd53e81fd82
ScottiBR <guilhermescotti@gmail.com>
2019-10-02 22:15:56 -0300

    Refactoring main class with react hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import SHOP_DATA from './shop.data'
+import SHOP_DATA from "./shop.data";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
+const ShopPage = props => {
+  const [collections] = useState({ collections: SHOP_DATA });
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
-    render() {
-        const { collections } = this.state
-        return (<div className='shop-page'>
-            {
-                collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
-
-        </div>)
-    }
-}
-
-export default ShopPage
+export default ShopPage;
